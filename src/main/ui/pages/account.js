eb644e293f0093eb214273a790b3807ed94ace23
parkseoh <parkseoh@iu.edu>
2023-04-20 12:44:04 -0400

    temp

---
+++ src/main/ui/pages/account.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function account() {
+    return (
+        <>
+            <h1>Account Page</h1>
+        </>
+    )
+}
