9f793a1a868cbe8f4ed18af12a026574807e63f3
Ethan <etmeyers@iu.edu>
2023-04-25 13:20:59 -0400

    Example API calls

--- src/main/ui/pages/api/hello.js
+++ src/main/ui/pages/api/hello.js
@@ -1,5 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+  //Example API Requests
+
+  let host = "http://localhost:8080";
+
+  let findAllAccounts = () => {
+    return fetch(host + '/accounts')
+        .then(x => x.json());
+  };
+
+  let saveLogin = (login) => {
+    let createURL = "/create/"+login.email+"/"+login.password+"/false";
+
+    return fetch(host + createURL, {
+      method: 'POST',
+    }).then(response =>
+    {
+      if (response.status == 200 || response.status == 201) return response.json();
+      return null;
+    })
+        .then(id => id)
+        .catch(error => {
+          console.log(error);
+          return null;
+        });
+  }
+
+  //Example API Requests
+
 }
