c448878e319d068e95c941f921689dffefaa18c0
parkseoh <parkseoh@iu.edu>
2023-04-26 19:43:43 -0400

    account2

--- src/main/ui/pages/account.js
+++ src/main/ui/pages/account.js
@@ -3,79 +3,73 @@ import { useState, useEffect } from 'react';
 function BankAccountPage() {
     const [bankAccounts, setBankAccounts] = useState([]);
     const [selectedAccount, setSelectedAccount] = useState(null);
-    const [accountBalance, setAccountBalance] = useState(null);
-    const [transferAmount, setTransferAmount] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:8080/accounts")
             .then(response => response.json())
-            .then(data =>
-                {
-                    console.log(data);
-                    setBankAccounts(data);
-                }
-            )
+            .then(data => {
+                console.log(data);
+                setBankAccounts(data);
+            })
             .catch(error => console.error(error));
     }, []);
-
-    useEffect(() => {
-        if (selectedAccount) {
-            fetch("/create/{email}/{password}/{sendEmail}")
-                .then(response => response.json())
-                .then(data => setAccountBalance(data.balance))
-                .catch(error => console.error(error));
-        }
-    }, [selectedAccount]);
 
     function handleAccountSelect(account) {
         setSelectedAccount(account);
     }
 
-    function handleTransferAmountChange(event) {
-        setTransferAmount(event.target.value);
-    }
-
-    function handleTransferSubmit(event) {
-        event.preventDefault();
-
-        if (selectedAccount && transferAmount) {
-            fetch("/transfer/{recipientEmail}/{myEmail}/{myPassword}/{amount}", {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ amount: transferAmount })
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Transfer successful');
-                    setSelectedAccount(selectedAccount);
-                })
-                .catch(error => console.error(error));
+    function handleTransferClick() {
+        if (selectedAccount) {
+            window.location.href = '/transfer';
         }
     }
 
     return (
         <div>
             <h1>Bank Accounts</h1>
-            <ul>
-                {bankAccounts.map(account => (
-                    <li key={account.id} onClick={() => handleAccountSelect(account)}>
-                        {account.id}
-                    </li>
-                ))}
-            </ul>
-            {selectedAccount && (
-                <div>
-                    <h2>{selectedAccount.name}</h2>
-                    <p>Balance: {accountBalance}</p>
-                    <form onSubmit={handleTransferSubmit}>
-                        <label>
-                            Transfer Amount:
-                            <input type="number" value={transferAmount} onChange={handleTransferAmountChange} />
-                        </label>
-                        <button type="submit">Transfer</button>
-                    </form>
-                </div>
-            )}
+            <div style={{ border: '2px solid blue', padding: '10px' }}>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Balance</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {bankAccounts.map(account => (
+                        <tr key={account.id} onClick={() => handleAccountSelect(account)}>
+                            <td>{account.id}</td>
+                            <td>{account.name}</td>
+                            <td>{account.balance}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                {selectedAccount && (
+                    <div>
+                        <h2>{selectedAccount.name}</h2>
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Account ID</th>
+                                <th>Balance</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>{selectedAccount.id}</td>
+                                <td>{selectedAccount.balance}</td>
+                                <td>
+                                    <button onClick={handleTransferClick}>Transfer Money</button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
