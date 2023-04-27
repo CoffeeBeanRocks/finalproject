637ac7bf47e9a1010bbe525f11583fbdad4d8238
deeconp <dgprecht5352@gmail.com>
2023-04-26 20:42:43 -0400

    index.js file changes

--- src/main/ui/pages/index.js
+++ src/main/ui/pages/index.js
@@ -1,12 +1,37 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    return (
+      <html>
+      <head>
+          <h1>Banking System</h1>
+      </head>
+
+      <body>
+      <div className="login-form">
+          <form>
+              <h1>Login</h1>
+
+              <div className="content">
+                  <div className="input-field">
+                      <input type="email" placeholder="Email" autoComplete="nope"></input>
+                  </div>
+                  <div className="input-field">
+                      <input type="password" placeholder="Password" autoComplete="new-password"></input>
+                  </div>
+              </div>
+
+              <div className="action">
+                  <button>Register</button>
+                  <button>Sign in</button>
+              </div>
+          </form>
+      </div>
+      <script src="./script.js"></script>
+
+      </body>
+      </html>
   )
 }
